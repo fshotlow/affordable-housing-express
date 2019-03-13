48fcd0b5e4412aa7a181ca4c4e67dfbb746f6dfb
Frederick Shotlow <frederick.shotlow@austintexas.gov>
2019-03-13 06:15:35 -0500

    add debug info

--- database/db.js
+++ database/db.js
@@ -1,9 +1,15 @@
-require('dotenv').config()
+// require('dotenv').config()
+require('custom-env').env(true)
 const Sequelize = require('sequelize')
-const db={}
+const db = {}
+
+console.log(process.env.DB_HOST)
+console.log(process.env.DB_NAME)
+console.log(process.env.DB_USER)
+
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: "mysql",
+    dialect: process.env.DB_DIALECT,
     operatorsAliases: false,
     pool: {
         max: 5,
